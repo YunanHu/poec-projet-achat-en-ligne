8df0cac9505c7c6a734f54f8c82b048d342ee49f
isahraoui <ismailsahraoui11@gmail.com>
2019-11-19 15:59:16 +0100

    added sendEmail method on User class

--- eGameProject/src/main/java/org/eclipse/eGameProject/model/User.java
+++ eGameProject/src/main/java/org/eclipse/eGameProject/model/User.java
@@ -1,6 +1,15 @@
 package org.eclipse.eGameProject.model;
 
 import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class User {
 	private long UID;
@@ -73,5 +82,42 @@ public class User {
 		return firstName + " " + lastName.toUpperCase();
 	}
 	
+	public void sendEmail(String email) {
+		  final String username = "oplike304@gmail.com";
+	        final String password = "oplike91";
+
+	        Properties prop = new Properties();
+			prop.put("mail.smtp.host", "smtp.gmail.com");
+	        prop.put("mail.smtp.port", "587");
+	        prop.put("mail.smtp.auth", "true");
+	        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+	        
+	        Session session = Session.getInstance(prop,
+	                new javax.mail.Authenticator() {
+	                    protected PasswordAuthentication getPasswordAuthentication() {
+	                        return new PasswordAuthentication(username, password);
+	                    }
+	                });
+
+	        try {
+
+	            Message message = new MimeMessage(session);
+	            message.setFrom(new InternetAddress("oplike304@gmail.com"));
+	            message.setRecipients(
+	                    Message.RecipientType.TO,
+	                    InternetAddress.parse(email)
+	            );
+	            message.setSubject("(Projet) Confirmation de votre compte");
+	            message.setText("Cher Utilisateur,"+ "\n\n Cliquez sur ce lien pour activer votre compte : \n http://localhost:8080/testAuthProjet/activation");
+
+	            Transport.send(message);
+
+	            System.out.println("Done");
+
+	        } catch (MessagingException e) {
+	            e.printStackTrace();
+	        }
+			
+	}
 	
 }
