d521d73975b5aa330e280b99a0d1d14ad2da1869
Flo <ftrebalag@gmail.com>
2019-11-25 16:33:12 +0100

    Add Role class

---
+++ eGameProject/src/main/java/org/eclipse/eGameProject/model/Role.java
@@ -0,0 +1,56 @@
+package org.eclipse.eGameProject.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Table(name = "Role")
+public class Role implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(unique = true)
+	private String Rolename;
+	private String password;
+	@Column(unique = true)
+	private String email;
+	@Column(unique = true)
+	private String phone;
+	private String address;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "Role_role", joinColumns = @JoinColumn(name = "Role_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Collection<Role> roles = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private Role client;
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Role seller;
+	@ManyToOne
+	@JoinColumn(name = "admin_id")
+	private Role admin;
+}
+
+/*
+JDBC code for database
+
+List<Role> RolesList = new ArrayList<Role>();
+while (resultSet.next()) {
+            Role Role  = new Role();
+            Role.setId(resultSet.getInt("id"));
+            Role.setName(resultSet.getString("name"));
+            Role.setEmail(resultSet.getString("email"));
+            Role.setMobile(resultSet.getString("mobile"));
+            Role.setPassword(resultSet.getString("password"));
+            Role.setRole(resultSet.getString("role"));
+            Role.setStatus(resultSet.getString("status"));
+            Role.setLast_udpate(resultSet.getString("last_update"));
+
+            // print the results
+            System.out.println(Role);
+            RolesList.add(Role);
+        }
+
+*/
