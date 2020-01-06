3a9da7258750730f0ede7d3f25a84479a2f974d3
yunanhu <yunan.huhu@gmail.com>
2020-01-06 16:38:44 +0100

    Added billing and shipping address fields

--- EcGameProject/src/main/java/fr/EGame/projet/model/Address.java
+++ EcGameProject/src/main/java/fr/EGame/projet/model/Address.java
@@ -13,7 +13,21 @@ public class Address {
 	@Id
 	@GeneratedValue
 	private Long idAddress;
-	private String street;
-	private String zip;
-	private String city;
+
+	// creer les tables billingAddress et shippingAddress
+	private String billing_firstname;
+	private String billing_lastname;
+	private String billing_company;
+	private String billing_country;
+	private String billing_street;
+	private String billing_city;
+	private String billing_zipcode;
+	
+	private String shipping_firstname;
+	private String shipping_lastname;
+	private String shipping_company;
+	private String shipping_country;
+	private String shipping_street;
+	private String shipping_city;
+	private String shipping_zipcode;
 }
