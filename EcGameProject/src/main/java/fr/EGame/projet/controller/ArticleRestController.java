2818d12a95460b52158aef97108f9ec19e0939a3
yoann <yoann@yoann-pc>
2020-01-06 09:46:00 +0100

    wip modif initArticles

--- EcGameProject/src/main/java/fr/EGame/projet/controller/ArticleRestController.java
+++ EcGameProject/src/main/java/fr/EGame/projet/controller/ArticleRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,7 +43,7 @@ public class ArticleRestController {
 			return false;
 		}
 	}
-
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/initArticles")
 	public void initArticlesCategories() {
 
