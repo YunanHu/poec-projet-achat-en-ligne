a92a7dfbd1da6602a84cde5e7cfca2a5a85561f9
isahraoui <ismailsahraoui11@gmail.com>
2020-01-13 14:54:54 +0100

    loginreducer

--- EcGameShop/ciyashop_react/src/actions/loginInfo.js
+++ EcGameShop/ciyashop_react/src/actions/loginInfo.js
@@ -1,60 +1,65 @@
 // Action creator
 
 export const setRole = (role) => {
-    
-   return{
-        type: "SET_ROLE", 
-        role: role
-   }
+
+     return {
+          type: "SET_ROLE",
+          role: role
+     }
 }
 
 export const setUid = (uid) => {
-    
-    return{
-         type: "SET_UID", 
-         uid: uid
-    }
- }
 
- export const setLN = (lastname) => {
-    
-     return{
-          type: "SET_LN", 
+     return {
+          type: "SET_UID",
+          uid: uid
+     }
+}
+
+export const setLN = (lastname) => {
+
+     return {
+          type: "SET_LN",
           lastname: lastname
      }
-  }
+}
 
-  export const setFN = (firstname) => {
-    
-     return{
-          type: "SET_FN", 
+export const setFN = (firstname) => {
+
+     return {
+          type: "SET_FN",
           firstname: firstname
      }
-  }
+}
 
-  export const setPN = (phoneno) => {
-    
-     return{
-          type: "SET_PN", 
+export const setPN = (phoneno) => {
+
+     return {
+          type: "SET_PN",
           phoneno: phoneno
      }
-  }
+}
 
-  export const setEmail = (email) => {
-    
-     return{
-          type: "SET_EMAIL", 
+export const setEmail = (email) => {
+
+     return {
+          type: "SET_EMAIL",
           email: email
      }
-  }
-  
+}
 
-  
+export const setAddress = (address) => {
 
- export const logout = () => {
-    
-    return{
-         type: "LOGOUT", 
-         
-    }
- }
+     return {
+          type: "SET_ADDRESS",
+          address: address
+     }
+}
+
+export const logout = () => {
+
+     return {
+          type: "LOGOUT",
+
+     }
+}
