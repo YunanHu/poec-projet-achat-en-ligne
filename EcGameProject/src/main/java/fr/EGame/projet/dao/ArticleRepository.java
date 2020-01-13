a8798efb2f8b406c12808303f69590a147512b1c
yoannBiheux <ybiheux@gmail.com>
2020-01-13 12:38:18 +0100

    Modify ArticleRepository, ArticlerestController and Article

--- EcGameProject/src/main/java/fr/EGame/projet/dao/ArticleRepository.java
+++ EcGameProject/src/main/java/fr/EGame/projet/dao/ArticleRepository.java
@@ -3,8 +3,12 @@ package fr.EGame.projet.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.EGame.projet.model.Article;
+import fr.EGame.projet.model.Cart;
+import fr.EGame.projet.model.User;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 	
@@ -14,6 +18,10 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 	
 	Article findByArticlePlateforme(String plateforme);
 	List<Article> findByArticleDeactivated(boolean isDactivated);
+
+	
+	@Query("select c from Article c where c.user = :user")
+	List<Article> findByUser(@Param("user") User user);
 
 	
 } 
